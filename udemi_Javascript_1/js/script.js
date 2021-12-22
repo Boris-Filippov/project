02803b8bcd8335fe6c97e310d27250ead0bde0fd
Boris <filippovbor@gmail.com>
2021-12-22 15:52:38 +0300

    lesson_14_cicli

--- udemi_Javascript_1/js/script.js
+++ udemi_Javascript_1/js/script.js
@@ -1,63 +1,27 @@
 "use strict";
 
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('Error');
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;   
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }
-
-const num = 50;
-
-if (num < 50) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Много');
-} else {
-    console.log('Ok!');
-}
-
-(num === 50) ? console.log('Ok!') : console.log('Error');
-
-
-const num = 50;
-
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-        case 51:
-        console.log('В точку!');
-        break;
-    default:
-        console.log('не в этот раз');
-        break;          
-}
-
-const num = '50';
-
-switch (num) {
-    case '49':
-        console.log('Неверно');
-        break;
-    case '100':
-        console.log('Неверно');
-        break;
-        case '51':
-        console.log('В точку!');
-        break;
-    default:
-        console.log('не в этот раз');
-        break;          
-}
-
-
-
-
-
-
-
-
-
