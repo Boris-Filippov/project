b8ca14f6104a1585ae66176f426f7168b621c868
Boris <filippovbor@gmail.com>
2021-12-22 13:42:09 +0300

    esson_13_usloviya

--- udemi_Javascript_1/js/script.js
+++ udemi_Javascript_1/js/script.js
@@ -1,24 +1,58 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+const num = 50;
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его?', '');
+if (num < 50) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+(num === 50) ? console.log('Ok!') : console.log('Error');
 
-console.log(personalMovieDB);
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+        case 51:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;          
+}
+
+const num = '50';
+
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+        case '51':
+        console.log('В точку!');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;          
+}
+
 
 
 
