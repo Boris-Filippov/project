f0c77186170a2c5c4dff7c398602da4f65b84c09
Boris <filippovbor@gmail.com>
2021-12-24 14:10:58 +0300

    lesson_20_objects

--- udemi_Javascript_1/js/script.js
+++ udemi_Javascript_1/js/script.js
@@ -1,12 +1,40 @@
 'use strict';
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
 
-function done() {
-    console.log('Я прошел этот урок!');
-}
+options.makeTest();
 
-learnJS('Javascript', done);
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+
+// console.log(counter);
+
+console.log(Object.keys(options).length);
+
