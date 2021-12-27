80ee3500891063f861acbbf8df647aaa76a8456a
Boris <filippovbor@gmail.com>
2021-12-27 22:28:11 +0300

    lesson_21_massivi_i_psevdomassivi

--- udemi_Javascript_1/js/script.js
+++ udemi_Javascript_1/js/script.js
@@ -1,40 +1,71 @@
 'use strict';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log("Test");
-    }
-};
+// МАССИВЫ. МЕТОДЫ:
 
-options.makeTest();
+// * 1. Убираем последний элемент массива. Метод pop.
 
-const {border, bg} = options.colors;
-console.log(border);
+// const arr = [1, 2, 3, 6, 8];
 
-// console.log(options.name);
+// arr.pop();
 
-// delete options.name;
+// console.log(arr);
 
-// console.log(options);
 
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     }
+// * 2. Добавляем элемент в конце массива. Метод push.
+
+// const arr = [1, 2, 3, 6, 8];
+
+// arr.push(10);
+
+// console.log(arr);
+
+
+// * 3. Перебор элементов массива циклом length
+
+// const arr = [1, 2, 3, 6, 8, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
 
-// console.log(counter);
+// * Или аналогичный метод через for of:
 
-console.log(Object.keys(options).length);
+// const arr = [1, 2, 3, 6, 8, 10];
 
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+
+// // * 4. Метод forEach - используется для перебора массива:
+
+// const arr = [1, 2, 3, 6, 8, 10];
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// }); 
+
+
+// * 5. Метод split - формируем массив  из строк по порядку:
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products);
+
+// * 5.1. Метод join - делает из массива строку и метод sort сортирует данные в алфавитном порядке.
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort(); 
+// console.log(products.join('; '));
+
+
+// * 6. Сетод сортировки массива по порядку через функцию compareNum:
+
+// const arr = [1, 20, 13, 6, 18, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
