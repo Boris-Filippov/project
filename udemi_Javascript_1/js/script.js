fe9b4c237edc2cea93be930f6b6a6a51e1d960d0
Boris <filippovbor@gmail.com>
2021-12-28 19:31:23 +0300

    lesson_22_copy_objects

--- udemi_Javascript_1/js/script.js
+++ udemi_Javascript_1/js/script.js
@@ -1,71 +1,23 @@
 'use strict';
 
-// МАССИВЫ. МЕТОДЫ:
+// * 1. Копируем объект - метод 1.
 
-// * 1. Убираем последний элемент массива. Метод pop.
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
 
-// const arr = [1, 2, 3, 6, 8];
-
-// arr.pop();
-
-// console.log(arr);
+console.log(internet);
 
 
-// * 2. Добавляем элемент в конце массива. Метод push.
+// * 2. Копируем объект - метод 2.
 
-// const arr = [1, 2, 3, 6, 8];
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
 
-// arr.push(10);
+const num = [2, 5, 7];
 
-// console.log(arr);
+log(...num);
 
-
-// * 3. Перебор элементов массива циклом length
-
-// const arr = [1, 2, 3, 6, 8, 10];
-
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-
-// * Или аналогичный метод через for of:
-
-// const arr = [1, 2, 3, 6, 8, 10];
-
-// for (let value of arr) {
-//     console.log(value);
-// }
-
-
-// // * 4. Метод forEach - используется для перебора массива:
-
-// const arr = [1, 2, 3, 6, 8, 10];
-
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// }); 
-
-
-// * 5. Метод split - формируем массив  из строк по порядку:
-
-// const str = prompt("", "");
-// const products = str.split(", ");
-// console.log(products);
-
-// * 5.1. Метод join - делает из массива строку и метод sort сортирует данные в алфавитном порядке.
-
-// const str = prompt("", "");
-// const products = str.split(", ");
-// products.sort(); 
-// console.log(products.join('; '));
-
-
-// * 6. Сетод сортировки массива по порядку через функцию compareNum:
-
-// const arr = [1, 20, 13, 6, 18, 10];
-// arr.sort(compareNum);
-// console.log(arr);
-
-// function compareNum(a, b) {
-//     return a - b;
-// }
